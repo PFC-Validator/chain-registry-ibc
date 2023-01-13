3f6e0a6d11db088ac6ecf5a9a6c1832c961f8299
The PFC <81114960+PFC-developer@users.noreply.github.com>
2023-01-13 13:56:14 -0600

    feat: generate index file as well

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use std::{ fs};
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
+use std::fmt::Debug;
+use std::fs;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufReader, LineWriter, Write};
 use std::path::PathBuf;
 
-use error_chain::error_chain;
+use chrono::{DateTime, Utc};
 use clap::Parser;
+use error_chain::error_chain;
+
 use crate::chain::{Chain, ChainChannelInfo};
 use crate::ibc::IbcData;
 
@@ -38,7 +41,7 @@ fn main() -> Result<()> {
 
     let current_dir = PathBuf::from(args.in_directory);
 
-    let mut chains: HashMap<String, Chain> = HashMap::new();
+    let mut chains: BTreeMap<String, Chain> = BTreeMap::new();
 
     for entry in fs::read_dir(current_dir)? {
         let entry = entry?;
@@ -99,10 +102,37 @@ fn main() -> Result<()> {
         }
     }
     fs::create_dir_all(&args.out_directory)?;
+    let now: DateTime<Utc> = Utc::now();
+    let out_index_file = File::create(format!("{}/index.ts", &args.out_directory))?;
+    let mut out_line = LineWriter::new(out_index_file);
+
+    out_line.write(format!("// automatically generated: {}\n\n", now.to_rfc3339()).as_bytes())?;
+
+
     for chain in &chains {
+        out_line.write(format!("import {} from './{}.json';\n", &chain.0, &chain.0).as_bytes())?;
         let out_file = File::create(format!("{}/{}.json", &args.out_directory, chain.0))?;
         serde_json::to_writer_pretty(out_file, &chain.1)?;
     }
+    out_line.write("\nexport interface ChainDeets {
+  chain_name: string;
+  transfers: {
+    [chainId: string]: {
+      channel_src: string;
+      port_src: string;
+      channel_dest: string;
+      port_dest: string;
+      ordering: string;
+      version: string;
+    };
+  };
+}\n".as_bytes())?;
+    out_line.write(format!("export const ibcData: {{ [chainId:string]: ChainDeets }} = {{\n").as_bytes())?;
+    for chain in &chains {
+        out_line.write(format!("\t'{}':{},\n", &chain.0, &chain.0).as_bytes())?;
+    }
+    out_line.write(format!("}};\n").as_bytes())?;
+
     println!(
         "{} chain entries created in {:?}:", chains.len(),
         args.out_directory
